60ee2cef3ead5440f94e02a7cb9f1abd4c746cc4
Alison Andrade <alison.sandrade@hotmail.com>
2026-08-22 00:42:28 -0300

    refactor: add shared platform package

---
+++ internal/platform/response.go
@@ -0,0 +1,19 @@
+// Package platform domain-agnostic infrastructure (shared internally)
+package platform
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
+
+func ErrorJSON(w http.ResponseWriter, statusCode int, message string) {
+	JSON(w, statusCode, map[string]string{"error": message})
+}
